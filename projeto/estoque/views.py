758a7103a2bc10cc4912a3d5543338ea31258fca
Lucvs <lucas.braga@msn.com>
2021-02-19 03:49:54 -0300

    Saida: estoque_saida_list.html

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -5,17 +5,17 @@ from django.shortcuts import render, resolve_url
 from projeto.estoque.forms import EstoqueForm, EstoqueItensForm
 from projeto.produto.models import Produto
 from .forms import EstoqueForm, EstoqueItensForm
-from .models import Estoque, EstoqueItens
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 def estoque_entrada_list(request):
     template_name='estoque_entrada_list.html'
-    objects = Estoque.objects.filter(movimento='e')
+    objects = EstoqueEntrada.objects.all()
     context={'object_list': objects}
     return render(request, template_name, context)
 
 def estoque_entrada_detail(request, pk):
     template_name='estoque_entrada_detail.html'
-    obj = Estoque.objects.get(pk=pk)
+    obj = EstoqueEntrada.objects.get(pk=pk)
     context={'object': obj}
     return render(request, template_name, context)
 
@@ -31,8 +31,8 @@ def dar_baixa_estoque(form):
 
 def estoque_entrada_add(request):
     template_name='estoque_entrada_form.html'
-    estoque_form=Estoque()
-    item_estoque_formset = inlineformset_factory(Estoque, EstoqueItens, form=EstoqueItensForm, extra=0, min_num=1, validate_min=True)
+    estoque_form=EstoqueEntrada()
+    item_estoque_formset = inlineformset_factory(EstoqueEntrada, EstoqueItens, form=EstoqueItensForm, extra=0, min_num=1, validate_min=True)
     if request.method == 'POST':
         form = EstoqueForm(request.POST, instance=estoque_form,prefix='main')
         formset = item_estoque_formset(request.POST, instance=estoque_form, prefix='estoque')
@@ -49,4 +49,9 @@ def estoque_entrada_add(request):
     context = {'form': form, 'formset': formset}
     return render(request, template_name, context)
 
-# Create your views here.
+
+def estoque_saida_list(request):
+    template_name='estoque_saida_list.html'
+    objects = EstoqueSaida.objects.all()
+    context={'object_list': objects}
+    return render(request, template_name, context)
